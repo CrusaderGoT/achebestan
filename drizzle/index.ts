a82e2c6c35df5f2eec9746657de0e0c52cc6da5c
Emeka <133652764+CrusaderGoT@users.noreply.github.com>
2025-07-21 01:36:23 +0100

    Update index.ts
    update database url readings

--- drizzle/index.ts
+++ drizzle/index.ts
@@ -1,6 +1,8 @@
 import { drizzle } from "drizzle-orm/neon-http";
 
 export const db = drizzle({
-    connection: process.env.LOCAL_POSTGRES_URL as string,
+    connection: process.env.NODE_ENV === "production"
+        ? process.env.POSTGRES_URL as string
+        : process.env.LOCAL_POSTGRES_URL as string,
     casing: "snake_case",
 });
