5d8fc85790333d168d8210054dfc55757ceed009
CrusaderGoT <enememeka44@gmail.com>
2025-07-24 09:21:16 +0100

    fixing deploy issues

--- lib/actions/book.ts
+++ lib/actions/book.ts
@@ -4,12 +4,9 @@ import { bookInsertSchema } from "@/zod-schemas/book";
 import { actionClient } from "../safe-action";
 
 export const createBookAction = actionClient
-    .metadata({ actionName: "createBookAction", user: "anon" })
     .inputSchema(bookInsertSchema)
     .action(async ({ parsedInput: { ...inputData } }) => {
         return {
             message: `${inputData}`,
         };
     });
-
-    
--- lib/safe-action.ts
+++ lib/safe-action.ts
@@ -1,10 +1,10 @@
 import { NeonDbError } from "@neondatabase/serverless";
 import { createSafeActionClient } from "next-safe-action";
+import { PostgresError } from "postgres";
 
 export const actionClient = createSafeActionClient({
-   
     async handleServerError(e) {
-        if (e instanceof NeonDbError) {
+        if (e instanceof NeonDbError || PostgresError) {
             console.error(e);
 
             return "a database error occured";
@@ -13,4 +13,3 @@ export const actionClient = createSafeActionClient({
         return e.message;
     },
 });
-
