bb221c4bcf0fb22d9ad2541db66fb3ff96b19289
CrusaderGoT <enememeka44@gmail.com>
2025-09-24 05:36:16 +0100

    "version": "1.0.1",

--- components/story/story-page.tsx
+++ components/story/story-page.tsx
@@ -22,7 +22,7 @@ import { StoryTitle } from "@/components/story/story-title";
 import { authClient } from "@/lib/auth-client";
 import { useUpdateStory } from "@/lib/hooks/story/update-story-hook";
 
-import { useDisclosure, useMounted } from "@mantine/hooks";
+import { useDisclosure } from "@mantine/hooks";
 import { IconPhotoEdit } from "@tabler/icons-react";
 import { zod4Resolver } from "mantine-form-zod-resolver";
 
@@ -164,10 +164,6 @@ export function Story({
     }
 
     const session = authClient.useSession();
-
-    const mounted = useMounted();
-
-    if (!mounted) return null;
 
     return (
         <UpdateStoryFormProvider form={form}>
