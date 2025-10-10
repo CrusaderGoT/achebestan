663b3f7119c10ee79aeb979de130bb334caea269
CrusaderGoT <enememeka44@gmail.com>
2025-10-10 02:15:39 +0100

    fixing migration sql conflict with production

--- drizzle/migrations/0032_blushing_salo.sql
+++ drizzle/migrations/0032_blushing_salo.sql
@@ -1,4 +1,4 @@
-CREATE TABLE "invitation" (
+CREATE TABLE IF NOT EXISTS  "invitation" (
 	"id" text PRIMARY KEY NOT NULL,
 	"organization_id" text NOT NULL,
 	"email" text NOT NULL,
@@ -8,7 +8,7 @@ CREATE TABLE "invitation" (
 	"inviter_id" text NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE "member" (
+CREATE TABLE IF NOT EXISTS  "member" (
 	"id" text PRIMARY KEY NOT NULL,
 	"organization_id" text NOT NULL,
 	"user_id" text NOT NULL,
@@ -16,7 +16,7 @@ CREATE TABLE "member" (
 	"created_at" timestamp NOT NULL
 );
 --> statement-breakpoint
-CREATE TABLE "organization" (
+CREATE TABLE IF NOT EXISTS  "organization" (
 	"id" text PRIMARY KEY NOT NULL,
 	"name" text NOT NULL,
 	"slug" text,
@@ -26,8 +26,8 @@ CREATE TABLE "organization" (
 	CONSTRAINT "organization_slug_unique" UNIQUE("slug")
 );
 --> statement-breakpoint
-ALTER TABLE "session" ADD COLUMN "active_organization_id" text;--> statement-breakpoint
-ALTER TABLE "invitation" ADD CONSTRAINT "invitation_organization_id_organization_id_fk" FOREIGN KEY ("organization_id") REFERENCES "public"."organization"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "invitation" ADD CONSTRAINT "invitation_inviter_id_user_id_fk" FOREIGN KEY ("inviter_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "member" ADD CONSTRAINT "member_organization_id_organization_id_fk" FOREIGN KEY ("organization_id") REFERENCES "public"."organization"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "member" ADD CONSTRAINT "member_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
+ALTER TABLE "session" ADD COLUMN IF NOT EXISTS "active_organization_id" text;--> statement-breakpoint
+ALTER TABLE "invitation" ADD CONSTRAINT IF NOT EXISTS "invitation_organization_id_organization_id_fk" FOREIGN KEY ("organization_id") REFERENCES "public"."organization"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "invitation" ADD CONSTRAINT IF NOT EXISTS "invitation_inviter_id_user_id_fk" FOREIGN KEY ("inviter_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "member" ADD CONSTRAINT IF NOT EXISTS "member_organization_id_organization_id_fk" FOREIGN KEY ("organization_id") REFERENCES "public"."organization"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "member" ADD CONSTRAINT IF NOT EXISTS "member_user_id_user_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
--- lib/auth/permissions.ts
+++ lib/auth/permissions.ts
@@ -1,26 +1,28 @@
+// Permissions >> resource owner
+// WARNING! THEY ARE TO BE THOUGHT IN INSTANCES OF ALL ASPECT OF THAT RESOURCE.
+// e.g, A DELTE PERM WILL ALLOW HAVERS TO DO WHAT IT PERMITS IRREGARDLESS OF RESOURCE OWNER
+// ALWAYS ACCOUNT FOR INSTANCE A ROLE SHOULD NOT HAVE ORGANIZATION WIDE PERMISSION
+
 import { createAccessControl } from "better-auth/plugins/access";
-import {
-    adminAc,
-    defaultStatements
-} from "better-auth/plugins/admin/access";
+import { adminAc, defaultStatements } from "better-auth/plugins/admin/access";
 
 const customPermissions = {
     ...defaultStatements,
-    story: ["create", "share", "update", "delete"],
+    story: ["create", "update", "delete"],
     organization: ["create", "update", "delete"],
+    comment: ["create", "update", "delete"],
 } as const;
 
 export const customAccessControl = createAccessControl(customPermissions);
 
-export const user = customAccessControl.newRole({
-    story: ["share"],
-});
+export const user = customAccessControl.newRole({});
 
 export const admin = customAccessControl.newRole({
-    story: ["create", "delete", "update", "share"],
     ...adminAc.statements,
+    story: [...customPermissions.story],
+    comment: [...customPermissions.comment],
 });
 
 export const writer = customAccessControl.newRole({
-    story: ["create", "delete", "update", "share"],
+    story: ["create"],
 });
