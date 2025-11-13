2226ab7002857f88efa2e9d7e06b32d8eb6f10a1
CrusaderGoT <enememeka44@gmail.com>
2025-11-13 20:54:19 +0100

    Reapply "upgraded serwist to work with turbo pack. and removed the deprecated new Serwist initialization process."
    This reverts commit 4e8afd50931d8d7dfbdcda1251c235dd0e82efcd.

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,16 @@
 import type { NextConfig } from "next";
 
-import withSerwistInit from "@serwist/next";
-
-const withSerwist = withSerwistInit({
-    swSrc: "app/sw.ts",
-    swDest: "public/sw.js",
-    cacheOnNavigation: true,
-    scope: "/",
-    register: true,
-    reloadOnOnline: true,
-    disable: process.env.NODE_ENV === "development",
-});
-
 const nextConfig: NextConfig = {
     basePath: "",
     reactStrictMode: true,
 <<<<<<< HEAD
+<<<<<<< HEAD
     serverExternalPackages: ["esbuild-wasm"],
 =======
 >>>>>>> 4e8afd5 (Revert "upgraded serwist to work with turbo pack. and removed the deprecated new Serwist initialization process.")
+=======
+    serverExternalPackages: ["esbuild", "@esbuild/linux-x64"],
+>>>>>>> e9c9aca (Reapply "upgraded serwist to work with turbo pack. and removed the deprecated new Serwist initialization process.")
     experimental: {
         optimizePackageImports: [
             "@mantine/core",
@@ -83,8 +75,17 @@ const nextConfig: NextConfig = {
                     },
                 ],
             },
+            {
+                source: "/:path*",
+                headers: [
+                    {
+                        key: "Service-Worker-Allowed",
+                        value: "/",
+                    },
+                ],
+            },
         ];
     },
 };
 
-export default withSerwist(nextConfig);
+export default nextConfig;
