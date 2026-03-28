007c54981dbd3a8f4afb6c402ce49bd86ba12951
CrusaderGoT <enememeka44@gmail.com>
2026-03-28 23:02:39 +0100

    fix sql create index error; check if index already exists

--- drizzle/migrations/0035_sticky_ultimates.sql
+++ drizzle/migrations/0035_sticky_ultimates.sql
@@ -1,3 +1,3 @@
-CREATE INDEX "account_userId_idx" ON "account" USING btree ("user_id");--> statement-breakpoint
-CREATE INDEX "session_userId_idx" ON "session" USING btree ("user_id");--> statement-breakpoint
-CREATE INDEX "verification_identifier_idx" ON "verification" USING btree ("identifier");
+CREATE INDEX IF NOT EXISTS "account_userId_idx" ON "account" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "session_userId_idx" ON "session" USING btree ("user_id");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "verification_identifier_idx" ON "verification" USING btree ("identifier");
