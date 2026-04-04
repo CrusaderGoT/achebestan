3a2c29bbb4be465d70263290232fc2b0424d830e
Emeka <133652764+CrusaderGoT@users.noreply.github.com>
2026-04-04 03:32:00 +0100

    Update 0041_gorgeous_prowler.sql
    fixing sql

--- drizzle/migrations/0041_gorgeous_prowler.sql
+++ drizzle/migrations/0041_gorgeous_prowler.sql
@@ -1,7 +1,10 @@
-SELECT conname, contype
-FROM pg_constraint
-WHERE conrelid = 'stories'::regclass;
-
-SELECT indexname, indexdef
-FROM pg_indexes
-WHERE tablename = 'stories';
+ALTER TABLE "stories" DROP CONSTRAINT IF EXISTS "stories_bookPart_unique";
+ALTER TABLE "stories" DROP CONSTRAINT IF EXISTS "stories_isbn_bookPart_unique";
+DO $$
+BEGIN
+  IF NOT EXISTS (
+    SELECT 1 FROM pg_constraint WHERE conname = 'stories_book_id_book_part_uidx'
+  ) THEN
+    ALTER TABLE "stories" ADD CONSTRAINT "stories_book_id_book_part_uidx" UNIQUE("book_id","book_part");
+  END IF;
+END $$;
